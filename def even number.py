756d5737eb444c305b8bd6b85d5140c1f5c2f9fb
saipavan6 <78468012+saipavan6@users.noreply.github.com>
2021-02-03 15:14:28 +0530

    def
    using functions

---
+++ def even number.py
@@ -0,0 +1,8 @@
+def even (number):
+    if number % 2 == 0:
+        print("given number is even")
+    else :
+        print("not even number")
+
+number = int(input("enter a number :"))
+even(number)
